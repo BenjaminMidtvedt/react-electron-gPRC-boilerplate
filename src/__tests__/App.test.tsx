50f8dde7f91d2d5b905e76ff4b7acc30f5724ba4
Benjamin Midtvedt <benmid@student.chalmers.se>
2022-01-03 00:05:11 +0100

    Install and compile grpc

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,14 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../renderer/App';
+import * as Routes from '../compiled/routes';
 
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
+  });
+
+  it('Should compile proto', () => {
+    expect(Routes).toBeDefined();
   });
 });
