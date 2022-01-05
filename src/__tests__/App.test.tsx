af17f3f0a31f3a5adab417c3d1d16021cecd4352
Benjamin Midtvedt <benmid@student.chalmers.se>
2022-01-05 05:40:09 +0100

    fix lint

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,8 +3,6 @@ import { render } from '@testing-library/react';
 import App from '../renderer/App';
 import * as Routes from '../compiled/routes';
 
-import client from '../main/client';
-
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
